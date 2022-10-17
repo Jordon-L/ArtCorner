c5126c8e2ceb125d072e95d6904d7fdce4d1ed45
Jordon-L <jordonlee1412@gmail.com>
2022-10-16 22:26:31 -0700

    gallery

--- nav.js
+++ nav.js
@@ -1,10 +1,23 @@
-// Toggle navigation open 
+// Toggle navigation open
 
 const navbarMenu = document.querySelector("#navigation #navbar-menu");
 const hamburgerMenu = document.querySelector("#navigation .hamburger-menu");
 const main = document.querySelector("main");
+const gallery = document.querySelector(".gallery");
 
-hamburgerMenu.addEventListener('click', function() {
-    navbarMenu.classList.toggle("open");
-    hamburgerMenu.classList.toggle("open");
+hamburgerMenu.addEventListener("click", function () {
+  navbarMenu.classList.toggle("open");
+  hamburgerMenu.classList.toggle("open");
 });
+
+function getImages() {
+  let html = "";
+  for(let i = 1; i < 9; i++){
+    html += `    <picture>
+    <img src="src/images/trending/${i}.jpg">
+    </picture>` ;      
+  }
+  gallery.innerHTML = html;
+};
+
+getImages();
