8cf6152b1dec5b374610d14ee6da5a6cd292340f
Jordon-L <jordonlee1412@gmail.com>
2022-10-25 19:10:32 -0700

    get images from pocketbase

--- src/typescript/gallery.ts
+++ src/typescript/gallery.ts
@@ -1,12 +1,24 @@
+import PocketBase from "pocketbase";
 
+const client = new PocketBase("https://www.pocketbase.jordonlee.com");
 
-function setupGallery() {
+async function setupGallery() {
   const gallery = document.querySelector(".gallery");
+  const resultList = await client.records.getList('posts', 1, 50, {
+    filter: 'created >= "2022-01-01 00:00:00"',
+  });
+  const records = await client.records.getFullList('posts', 200 /* batch size */, {
+    sort: '-created',
+});
+	//api/files/dxkfd1u81k4ela0/ymi90taml49uco8/monkey_lzCJt0iVrM.jpg?thumb=100x100
+  console.log(resultList);
+  console.log(records );
   if(gallery != null){
     let html = "";
-    for(let i = 1; i < 9; i++){
+    for(let i = 0; i < resultList.totalItems; i++){
+      const post = resultList.items[i];
       html += `    <picture>
-      <img src="../images/trending/${i}.jpg">
+      <img src= https://www.pocketbase.jordonlee.com/api/files/${post["@collectionId"]}/${post["id"]}/${post["file"]}?thumb=200x200>
       </picture>` ;      
     }
       gallery.innerHTML = html;
