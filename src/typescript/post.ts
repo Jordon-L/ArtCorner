cd62e19f0dbb51eb8a118d30779950b51e7adc35
Jordon-L <jordonlee1412@gmail.com>
2022-11-03 01:03:29 -0700

    vite build error

--- src/typescript/post.ts
+++ src/typescript/post.ts
@@ -4,10 +4,16 @@ import {updateImage} from "./gallery"
 
 const client = new PocketBase("https://www.pocketbase.jordonlee.com");
 
-const searchParams = new URLSearchParams(window.location.search);
-const id = searchParams.get('id');
-if(id != null){
-  console.log(id);
-  const post = await client.records.getOne('posts', id, {});
-  updateImage(post);
+
+async function getPost(){
+  const searchParams = new URLSearchParams(window.location.search);
+  const id = searchParams.get('id');
+  if(id != null){
+    console.log(id);
+    const post = await client.records.getOne('posts', id, {});
+    updateImage(post);
+  }
 }
+
+
+getPost();
